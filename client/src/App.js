6044b378f51cc15e17247629a60f2e1cb73ebf27
vinsdragonis <zrexteam128@gmail.com>
2021-12-17 21:10:59 +0530

    Minimal topbar created

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,9 @@ import Topbar from "./components/topbar/Topbar";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Topbar />
-      <h1>Project Nexus</h1>
-    </div>
+    </>
   );
 }
 
--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
 import './topbar.css';
+// import { Link } from 'react-router-dom';
 
 export default function Topbar() {
     return (
         <div className="top">
-            Topbar
+            <div className="topLeft">
+                <i className="topIcon fab fa-facebook-square"></i>
+                <i className="topIcon fab fa-instagram-square"></i>
+                <i className="topIcon fab fa-github-square"></i>
+                <i className="topIcon fab fa-reddit-square"></i>
+            </div>
+            <div className="topCenter">
+                <ul className="topList">
+                    <li className="topListItem">HOME</li>
+                    <li className="topListItem">ABOUT</li>
+                    <li className="topListItem">CONTACT</li>
+                    <li className="topListItem">WRITE</li>
+                </ul>
+            </div>
+            <div className="topRight">
+                <ul className="topList">
+                    <li className="topListItem">LOGIN</li>
+                    <li className="topListItem">REGISTER</li>
+                </ul>
+                <i className="topSearchIcon fas fa-search"></i>
+            </div>
         </div>
     )
 }
