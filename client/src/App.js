7f3a652e7d94296720ad7d14d7f6d76b9ab7f1db
vinsdragonis <zrexteam128@gmail.com>
2021-12-19 05:13:59 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,32 @@
 import Homepage from './pages/homepage/Homepage';
-// import Individual from './pages/individual/Individual';
-// import Write from "./pages/write/Write";
-// import Settings from "./pages/settings/Settings";
-// import Login from "./pages/login/Login";
-// import Register from "./pages/register/Register";
+import Individual from './pages/individual/Individual';
+import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import Topbar from "./components/topbar/Topbar";
+import {
+  BrowserRouter as
+  Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
+  const currentUser = true;
   return (
-    <>
+    <Router>
       <Topbar />
-      <Homepage />
-      {/* <Individual /> */}
-      {/* <Write /> */}
-      {/* <Settings /> */}
-      {/* <Login /> */}
-      {/* <Register /> */}
-    </>
+      <Routes>
+        <Route exact path="/" element={ <Homepage /> } />
+        <Route exact path="/posts" element={ <Homepage /> } />
+        <Route path="/post/:id" element={ <Individual /> } />
+        <Route path="/write" element={ currentUser ? <Write /> : <Login /> } />
+        <Route path="/settings" element={ currentUser ? <Settings /> : <Login /> } />
+        <Route path="/login" element={ currentUser ? <Homepage /> : <Login /> } />
+        <Route path="/register" element={ currentUser ? <Homepage /> : <Register /> } />
+      </Routes>
+    </Router>
   );
 }
 
