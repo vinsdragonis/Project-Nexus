7884cb88f2513f258a747270a6a3b30d92bec60d
vinsdragonis <zrexteam128@gmail.com>
2021-12-19 13:25:48 +0530

    Update Topbar.jsx

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -2,6 +2,7 @@ import './topbar.css';
 import { Link } from 'react-router-dom';
 
 export default function Topbar() {
+    const user = true;
     return (
         <div className="top">
             <div className="topLeft">
@@ -24,25 +25,28 @@ export default function Topbar() {
                     <Link className="link" to="/write" >
                         <li className="topListItem">WRITE</li>
                     </Link>
-                    <li className="topListItem">LOGOUT</li>
+                    { user && <li className="topListItem">LOGOUT</li> }
                 </ul>
             </div>
             <div className="topRight">
-                <Link className="link" to="/settings">
-                    <img
-                        className="topImg"
-                        src="https://avatars.githubusercontent.com/u/37930821?v=4"
-                        alt=""
-                    />
-                </Link>
-                <ul className="topList">
-                    <Link className="link" to="/login">
-                        <li className="topListItem">LOGIN</li>
+                { user ? (
+                    <Link className="link" to="/settings">
+                        <img
+                            className="topImg"
+                            src="https://wallpapercave.com/wp/wp9584059.jpg"
+                            alt=""
+                        />
                     </Link>
-                    <Link className="link" to="/register">
-                        <li className="topListItem">REGISTER</li>
-                    </Link>
-                </ul>
+                ) : (
+                    <ul className="topList">
+                        <Link className="link" to="/login">
+                            <li className="topListItem">LOGIN</li>
+                        </Link>
+                        <Link className="link" to="/register">
+                            <li className="topListItem">REGISTER</li>
+                        </Link>
+                    </ul>
+                )}
                 <i className="topSearchIcon fas fa-search"></i>
             </div>
         </div>
