681f1884af6224fd88be4c74449cc65caa885829
vinsdragonis <zrexteam128@gmail.com>
2021-12-20 23:20:19 +0530

    Finished routes

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
-// const categoryRoute = require("./routes/categories");
+const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
 
@@ -30,6 +30,7 @@ mongoose
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("5000", () => {
   console.log("Backend server is running.");
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -73,8 +73,10 @@ router.get("/:id", async (req, res) => {
 router.get("/", async (req, res) => {
     const username = req.query.user;
     const catName = req.query.cat;
+
     try {
         let posts;
+        
         if (username) {
             posts = await Post.find({ username });
         } else if (catName) {
@@ -86,6 +88,7 @@ router.get("/", async (req, res) => {
         } else {
             posts = await Post.find();
         }
+
         res.status(200).json(posts);
     } catch (err) {
         res.status(500).json(err);
