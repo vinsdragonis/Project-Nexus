dfe1a331467c0f08cf68d530d457d4e52a986c90
vinsdragonis <zrexteam128@gmail.com>
2021-12-23 00:00:18 +0530

    Updated individualPost.jsx

--- client/src/components/indvidualPost/IndividualPost.jsx
+++ client/src/components/indvidualPost/IndividualPost.jsx
@@ -1,28 +1,50 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useLocation } from "react-router";
 import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
 import './individualPost.css';
 
 export default function IndividualPost() {
     const location = useLocation();
     const path = location.pathname.split("/")[2];
     const PF = "http://localhost:5000/images/";
-    const [post, setPost] = useState({});  
-    // const [title, setTitle] = useState("");
-    // const [desc, setDesc] = useState("");
-    // const [updateMode, setUpdateMode] = useState(false);
+    const { user } = useContext(Context);
+    const [post, setPost] = useState({});
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
+    const [updateMode, setUpdateMode] = useState(false);
 
     useEffect(() => {
         const getPost = async () => {
             const res = await axios.get("http://localhost:5000/api/posts/" + path);
             setPost(res.data);
-            // setPost(res.data);
-            // setTitle(res.data.title);
-            // setDesc(res.data.desc);
+            setPost(res.data);
+            setTitle(res.data.title);
+            setDesc(res.data.desc);
         };
         getPost();
-    }, [path])
+    }, [path]);
+
+    const handleDelete = async () => {
+        try {
+        await axios.delete(`/posts/${post._id}`, {
+            data: { username: user.username },
+        });
+        window.location.replace("/");
+        } catch (err) {}
+    };
+
+    const handleUpdate = async () => {
+        try {
+        await axios.put(`/posts/${post._id}`, {
+            username: user.username,
+            title,
+            desc,
+        });
+        setUpdateMode(false)
+        } catch (err) {}
+    };
 
     return (
         <div>
@@ -32,14 +54,33 @@ export default function IndividualPost() {
                         className="indPostImg"
                         src={ PF + post.photo }
                         alt=""
-                    />}
-                <h1 className="indPostTitle">
-                    { post.title }
-                    <div className="indPostEdit">
-                        <i className="indPostIcon far fa-edit"></i>
-                        <i className="indPostIcon far fa-trash-alt"></i>
-                    </div>
-                </h1>
+                    />
+                }
+                { updateMode ? (
+                    <input
+                        type="text"
+                        value={title}
+                        className="singlePostTitleInput"
+                        autoFocus
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                ) : (
+                    <h1 className="indPostTitle">
+                        { title }
+                         {post.username === user?.username && (
+                        <div className="indPostEdit">
+                            <i
+                            className="indPostIcon far fa-edit"
+                            onClick={() => setUpdateMode(true)}
+                            ></i>
+                            <i
+                            className="indPostIcon far fa-trash-alt"
+                            onClick={handleDelete}
+                            ></i>
+                        </div>
+                        )}
+                    </h1>
+                )}
                 <div className="indPostInfo">
                     <span>
                         Author:
@@ -51,9 +92,20 @@ export default function IndividualPost() {
                     </span>
                     <span>{ new Date(post.createdAt).toDateString() }</span>
                 </div>
-                <p className="indPostDesc">
-                    { post.desc }
-                </p>
+                {updateMode ? (
+                    <textarea
+                        className="indPostDescInput"
+                        value={desc}
+                        onChange={(e) => setDesc(e.target.value)}
+                    />
+                    ) : (
+                        <p className="indPostDesc">{desc}</p>
+                    )}
+                    {updateMode && (
+                    <button className="indPostButton" onClick={handleUpdate}>
+                        Update
+                    </button>
+                )}
             </div>
         </div>
     )
