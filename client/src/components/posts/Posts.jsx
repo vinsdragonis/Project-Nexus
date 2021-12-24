390c0b370401cfc909f11b9987c4b229e8d4c3c7
vinsdragonis <zrexteam128@gmail.com>
2021-12-24 12:01:13 +0530

    Update Posts.jsx

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -4,7 +4,7 @@ import './posts.css';
 export default function Posts({ posts }) {
     return (
         <div className="posts">
-            { posts.map((p) => (
+            { posts.slice().reverse().map((p) => (
                 <Post key={ p._id } post={ p } />
             ))}
         </div>
