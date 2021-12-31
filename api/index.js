c0b4b8408ef2af0da145bae1897d53ec1860b373
vinsdragonis <zrexteam128@gmail.com>
2021-12-31 19:23:30 +0530

    .

--- api/index.js
+++ api/index.js
@@ -15,7 +15,6 @@ const PORT =  process.env.PORT || 5000;
 dotenv.config();
 app.use(express.json());
 app.use(cors());
-// app.use("/images", express.static(path.join(__dirname, "/images")));
 
 mongoose
     .connect(
