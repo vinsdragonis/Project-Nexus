58e0eef0c58d746a77a239550cd9a202c88eae18
vinsdragonis <zrexteam128@gmail.com>
2021-12-31 15:57:58 +0530

    Update Individual.jsx

--- client/src/pages/individual/Individual.jsx
+++ client/src/pages/individual/Individual.jsx
@@ -1,11 +1,13 @@
 import Sidebar from '../../components/sidebar/Sidebar';
 import IndividualPost from '../../components/indvidualPost/IndividualPost';
+import Categories from '../../components/categories/Categories';
 import './individual.css';
 
 export default function Individual() {
 
     return (
         <div className="individual">
+            <Categories />
             <IndividualPost />
             <Sidebar />
         </div>
