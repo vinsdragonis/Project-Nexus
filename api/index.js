a20c93bdef1fda51383ea2a1232d973f5d14ddca
vinsdragonis <zrexteam128@gmail.com>
2022-01-01 16:10:41 +0530

    .

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const multer = require("multer");
 const cors = require("cors");
 const path = require("path");
 
-const PORT =  process.env.PORT || 5000;
+const port =  process.env.PORT || 5000;
 
 dotenv.config();
 app.use(express.json());
@@ -50,6 +50,6 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log("Backend server is running.");
 });
