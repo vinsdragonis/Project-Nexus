6623aae0c1e0afea696c8717d67bd9fbfbc371ea
Vikash Jajra <96776232+vikashjajra@users.noreply.github.com>
2022-08-30 23:21:22 +0530

    Figuring out vulnerabilities

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -8,7 +8,7 @@ router.post("/", async (req, res) => {
     const newPost = new Post(req.body);
     try {
         const savedPost = await newPost.save();
-        let user = await User.findOneAndUpdate({_id: req.body.user}, {$push: {posts: newPost}});
+        let user = await User.findOneAndUpdate({_id: $eq{req.body.user}}, {$push: {posts: newPost}});
         res.status(200).json(savedPost);
     } catch (err) {
         // res.status(500).json(err);
