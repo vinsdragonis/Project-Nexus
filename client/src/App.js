fa7fc91e420cf7e571124fe02463f64f73ddfb22
vinsdragonis <zrexteam128@gmail.com>
2023-01-07 21:00:00 +0530

    .

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <div className="app">
-      <Router>
+      <Router basename="/">
         <Topbar />
         <Routes>
           <Route exact path="/" element={ <Homepage /> } />
