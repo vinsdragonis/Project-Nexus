1398713cd4c6feaac72b3815dc4f7c60b61362e3
Vineeth B V <37930821+vinsdragonis@users.noreply.github.com>
2023-01-16 03:03:10 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,6 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
-const multer = require("multer");
 const cors = require("cors");
 const path = require("path");
 
@@ -31,20 +30,6 @@ mongoose
     .catch((err) => {
         console.log(err + "\nError brrr :(")
     });
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "images");
-    },
-    filename: (req, file, cb) => {
-        cb(null, req.body.name);
-    },
-});
-
-const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
-    res.status(200).json("File has been uploaded");
-});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
